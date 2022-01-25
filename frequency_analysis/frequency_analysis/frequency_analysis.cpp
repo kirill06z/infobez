7dc6b0846857f053580a35fb770cdb4d85a2da4d
kirill06z <98234884+kirill06z@users.noreply.github.com>
2022-01-25 10:08:36 +0300

    Add files via upload

---
+++ frequency_analysis/frequency_analysis/frequency_analysis.cpp
@@ -0,0 +1,118 @@
+﻿// frequency_analysis.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <ctype.h>
+
+using namespace std;
+  
+char text[1000];
+char text_reg[1000];
+char alf[] = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+char txt[1000];
+char n[32];
+
+int quen_symb = 0;
+int alf_num[32];
+int i=0;
+void raschet();
+void podschet();
+void perevod();
+int main()
+{
+    raschet();
+   
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+   
+    setlocale(LC_ALL, "Russian");
+  
+       
+        
+        for (int h = 0; h < 1000;h++) {
+            text[h] = tolower(text[h]);
+            cout << text[h];
+        }
+        
+    podschet();
+    cout << "zapusk perevod";
+    perevod();
+}
+
+void raschet() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+   
+    cout << "Введите текст" << endl;
+    cin.getline(text, 1000);
+    while (1) {
+        if (text[i] != '\0') {
+            if (text[i] == ' ' || text[i] == ',' || text[i] == '.' || text[i]=='!' || text[i]=='?') {
+                i++;
+            }
+
+            else {
+                quen_symb++;
+                i++;
+            }
+        }
+        
+        else { cout << "!"<<endl; break; }
+    }
+    cout << quen_symb;
+    
+}
+
+void podschet() {
+    int t = 0;
+    while (text[t] != '\0') {
+        for (int x = 0; x < 33; x++) {
+            if (text[t]==alf[x]) {
+                alf_num[x] = alf_num[x] + 1;
+
+            }
+        }
+       
+        
+        t++;
+
+ }
+   for (int x = 0; x < 33; x++) {
+        cout <<alf[x]<< alf_num[x]<<endl;
+    }
+  
+}
+void perevod() {
+    
+
+
+    for (int t = 0; t < 32; t++) {
+        int maxj = 0;
+        for (int j = 0; j < 1000; j++) {
+            if (alf_num[maxj] < alf_num[j]) alf_num[maxj] = alf_num[j];
+
+        }
+        n[t] += alf_num[maxj];
+        alf_num[maxj] = -1;
+        cout << n[t];
+    }
+    
+    /*int t = 0;
+    for (int k = 0;k<32;k++) {
+        for (int a = 0; a < 32;a++) {
+            
+
+        }
+
+    }
+    /*while (text[t] != '\0') {
+        for (int x = 0; x < 32; x++) {
+
+        }
+        t++;
+    }*/
+}
