025a5a92e32a466856c02be1069e5c99f0d2f126
kirill06z <98234884+kirill06z@users.noreply.github.com>
2022-01-25 21:38:23 +0300

    Add files via upload

--- frequency_analysis/frequency_analysis/frequency_analysis.cpp
+++ frequency_analysis/frequency_analysis/frequency_analysis.cpp
@@ -12,13 +12,18 @@ char text_reg[1000];
 char alf[] = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 char txt[1000];
 char n[32];
+char alf_pop[] = "оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфёъ";
+int symb;
+int pop_num[33];
 
 int quen_symb = 0;
-int alf_num[32];
+int alf_num[33];
+int alf_num1[33];
 int i=0;
 void raschet();
 void podschet();
 void perevod();
+
 int main()
 {
     raschet();
@@ -50,6 +55,7 @@ void raschet() {
     cout << "Введите текст" << endl;
     cin.getline(text, 1000);
     while (1) {
+        symb++;
         if (text[i] != '\0') {
             if (text[i] == ' ' || text[i] == ',' || text[i] == '.' || text[i]=='!' || text[i]=='?') {
                 i++;
@@ -86,64 +92,82 @@ void podschet() {
     }
   
 }
-/*void perevod() {
-    
+void perevod() {
 
 
-    for (int t = 0; t < 32; t++) {
-        int maxj = 0;
-        for (int j = 0; j < 1000; j++) {
-            if (alf_num[maxj] < alf_num[j]) alf_num[maxj] = alf_num[j];
 
-        }
-        n[t] += alf_num[maxj];
-        alf_num[maxj] = -1;
-        cout << n[t];
-    }
-    
-    /*int t = 0;
-    for (int k = 0;k<32;k++) {
-        for (int a = 0; a < 32;a++) {
-            
+    /*  for (int t = 0; t < 33; t++) {
+          int maxj = 0;
+          for (int j = 0; j < 1000; j++) {
+              if (alf_num[maxj] < alf_num[j]) alf_num[maxj] = alf_num[j];
 
-        }
-
-    }
-    /*while (text[t] != '\0') {
-        for (int x = 0; x < 32; x++) {
-
-        }
-        t++;
-    }*/
-}*/
-  
-  // добавить в 52 строку счетчик для подсчетов символов тексте (symb) 
-//добавить конечный файл текста text2;
-// добавить alf_num1 для сохранение оригинального порядка
-alf_pop[]="оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфёъ"
-char pop_num[32];
-void perevod(){
-
-for (int t=0;t<33;t++);
-{  for(int j;j<33;j++){
-   if(alf_num1[t]<alf_num[j]) alf_num1[t] = alf_num[j];
-
-}
-pop_num[t]+=alf_num1[t];
-alf_num1[t]=0;
-}
-cout<<pop_num;
-// по идее получим числа в порядке убывания
-// и теперь надо сделать цикл для перевода одного символа в другой
-for (int t=0; t<symb; t++){
-   for (int n=0; n<33;n++){
-    if (text[t]==alf[n]){
-     for(int x=0;x<32;x++){
-      if(alf_num[n]==num_pop[x])text2[t]=alf_pop[x];
+          }
+          n[t] += alf_num[maxj];
+          alf_num[maxj] = -1;
+          cout << n[t];
       }
-  
+
+      /*int t = 0;
+      for (int k = 0;k<32;k++) {
+          for (int a = 0; a < 32;a++) {
+
+
+          }
+
+      }
+      /*while (text[t] != '\0') {
+          for (int x = 0; x < 32; x++) {
+
+          }
+          t++;
+      }*/
+      // добавить в 52 строку счетчик для подсчетов символов тексте (symb) 
+  //добавить конечный файл текста text2;
+  // добавить alf_num1 для сохранение оригинального порядка
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251); 
+
+    for (int s = 0; s < 33; s++) {
+        alf_num1[s] = alf_num[s]; 
+        cout << alf_num1[s];
+        
     }
-   }
-}
-cout<<text2;
+  cout << endl;
+   
+  int num = 0;
+  int o = 0;
+        for ( int t = 0; t < 33; t++)
+        {
+            num = alf_num1[t];
+            for (int j=0; j < 33; j++) {
+           
+                if (num < alf_num1[j]) {
+                   num = alf_num1[j];
+                   o = j;
+                }
+        }
+        //cout << t;
+        pop_num[t] += num;
+        alf_num1[o] = -1;
+       cout << pop_num[t]<<" ";
+        }
+      
+        
+        // по идее получим числа в порядке убывания
+        // и теперь надо сделать цикл для перевода одного символа в другой
+        
+        for (int t = 0; t < symb; t++) {
+           
+            for (int n = 0; n < 33; n++) {
+                if (text[t] == alf[n]) {
+                    for (int x = 0; x < 32; x++) {
+                        if (alf_num[n] == pop_num[x])txt[t] = alf_pop[x];
+                    }
+
+                }
+            }
+            cout <<txt[t];
+        }
+       
+       // cout << symb;
 }
